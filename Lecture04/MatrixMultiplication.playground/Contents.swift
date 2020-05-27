e30120c74542b530d5e74c667a5f50ad17af69b1
EricGolovin <ericgolovin@outlook.com>
2020-05-28 02:54:13 +0300

    Added MatrixCalculator

--- Lecture04/MatrixMultiplication.playground/Contents.swift
+++ Lecture04/MatrixMultiplication.playground/Contents.swift
@@ -1,56 +1,89 @@
 import UIKit
 
-/*
-var firstMatrix = [[1, 2], [3, 4]]
-var secondMatrix = [[2, 0], [1, 2]]
-*/
+let maxMatrixElement = 10
+let numberOfQueues = 5
+let matrixSize = 10
 
-var firstMatrix = [Array<Int>]()
-var secondMatrix = [Array<Int>]()
-
-var resultMatrix = [Array<Int>]()
-
-for row in 0..<10 {
-    firstMatrix.append(Array<Int>())
-    secondMatrix.append(Array<Int>())
-    resultMatrix.append(Array<Int>())
-    for _ in 0..<10 {
-        firstMatrix[row].append(Int.random(in: 1...100))
-        secondMatrix[row].append(Int.random(in: 1...100))
-        resultMatrix[row].append(0)
-    }
+if matrixSize % 2 != 0 {
+    fatalError("Please, enter even matrix size")
 }
 
+enum MatrixContentOptions {
+    case zero
+    case random(Int)
+}
 
+func prepareMatrixesOf(size: Int, resultMatrix: inout [[Int]]) -> (first: [[Int]], second: [[Int]]) {
+    var result = (first: [[Int]](), second: [[Int]]())
+    
+    result.first = Array.make2D(with: .random(maxMatrixElement), size: size)
+    result.second = Array.make2D(with: .random(maxMatrixElement), size: size)
+    resultMatrix = Array.make2D(with: .zero, size: size)
+
+    return result
+}
+
+var queues = { (number: Int) -> Array<DispatchQueue> in
+    var result = Array<DispatchQueue>()
+    for i in 0..<number {
+        result.append(DispatchQueue(label: "queue\(i)", attributes: .concurrent))
+    }
+    return result
+}(numberOfQueues)
+
+var dispatchGroup = DispatchGroup()
+
+//var matrixes = (first: [[1, 2], [3, 4]], second: [[2, 0], [1, 2]])
+//var resultMatrix = Array.make2D(with: .zero, size: 2)
+
+var gcdTiming = 0.0
+var linearTiming = 0.0
+
+var resultMatrix = [[Int]]()
+var matrixes = prepareMatrixesOf(size: matrixSize, resultMatrix: &resultMatrix)
+
+resultMatrix.forEach { print($0) }
 
 // MARK: - Using GCD
+gcdTiming = CFAbsoluteTimeGetCurrent()
+for (i, row) in matrixes.first.enumerated() {
+    for j in 0..<row.count {
+        for k in 0..<row.count {
+//            print(k % queues.count)
+            queues[k % queues.count].async(group: dispatchGroup) {
+                resultMatrix[i][j] += matrixes.first[i][k] * matrixes.second[k][j]
+            }
+        }
+    }
+}
+gcdTiming = CFAbsoluteTimeGetCurrent() - gcdTiming
 
+dispatchGroup.notify(queue: .main) {
+    print("Result GCD:")
+    resultMatrix.forEach { print($0) }
+    resultMatrix = Array.make2D(with: .zero, size: matrixSize)
+    
+    // MARK: - Not using GCD
+    linearTiming = CFAbsoluteTimeGetCurrent()
+    for (i, row) in matrixes.first.enumerated() {
+        for (j, _) in row.enumerated() {
+            for k in 0..<row.count {
+                resultMatrix[i][j] += matrixes.first[i][k] * matrixes.second[k][j]
+            }
+        }
+    }
+    linearTiming = CFAbsoluteTimeGetCurrent() - linearTiming
+    print("Result Linear:")
+    resultMatrix.forEach { print($0) }
+    
+    print("\nGCD multiplication took \(gcdTiming)")
+    print("Linear multiplication took \(linearTiming)\n")
+}
 
-// MARK: - Non GCD
-//for row in 0..<firstMatrix.count {
-//    var subArray = [Int]()
-//    resultMatrix.append(Array<Int>())
-//    print("bufferMatrix.Count: \(resultMatrix.count) Row: \(row)")
-//
-//    for (secondMatrixRowIndex, _) in secondMatrix.enumerated() {
-//        for (index, num) in firstMatrix[row].enumerated() {
-//
-//            let numFromFirstMatrix = num
-//            let numFromSecondMatrix = secondMatrix[index][secondMatrixRowIndex]
-//
-//            let calulatedValue = numFromFirstMatrix * numFromSecondMatrix
-//            subArray.append(calulatedValue)
-//
-//            print("firstMatrix[\(row)][\(index)]_\(numFromFirstMatrix) * secondMatrix[\(index)][\(row)]_\(numFromSecondMatrix) = \(calulatedValue)\n")
-//
-//            if subArray.count == 2 {
-//                print("subArr[0] = \(subArray[0]), subArr[1] = \(subArray[1])")
-//                resultMatrix[row].append(subArray.first! + subArray.last!)
-//                subArray = []
-//            }
-//        }
-//    }
-//}
-
-
-print(resultMatrix)
+extension Array where Element == Int {
+    static func make2D(with option: MatrixContentOptions, size: Int) -> [[Int]] {
+        return (0..<size).map { _ in (0..<size).map { _ in
+            if case .random(let value) = option { return Int.random(in: 0...value) } else { return 0 }
+            } }
+    }
+}
